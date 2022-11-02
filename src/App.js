61c590e8804bf37f4a14f700091d13d577c20ded
unknown <you@example.com>
2022-11-01 18:42:47 -0600

    make it better

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <img src={comcaslogo} className="App-logo" alt="logo" />
-        <p>
+        <p className='nombreEmpresa'>
           COMCAS
         </p>
-        <p>
+        <p className='distribuidorAutorizado'>
          {/*Edit <code>src/App.js</code> and save to reload.*/}
          Distrribuidor Autorizado de Claro
         </p>
@@ -26,6 +26,18 @@ function App() {
         </a>
         */
         }
+        <p>
+          Contactanos Vía WhatsApp Con:
+        </p>
+        <a
+          className="App-link"
+          href="https://wa.me/+50581759532?text=Hola Jose, he visto la pagina de COMCAS y estoy interesado en los
+          servicios de Claro, me podrias dar mas informacion?."
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Jose Florez
+        </a>
       </header>
     </div>
   );
